9fa26ae0545c238187596f8d986f26e3ca2f1017
Victor Kling <162064340+VictorKLing@users.noreply.github.com>
2024-10-03 07:07:28 -0300

    Slide Resize

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,5 @@
+import debaunce from './debaunce.js';
+
 export default class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide);
@@ -70,11 +72,7 @@ export default class Slide {
         this.wrapper.addEventListener('touchend', this.onEnd);
     }
 
-    bindEvents() {
-        this.onStart = this.onStart.bind(this);
-        this.onMove = this.onMove.bind(this);
-        this.onEnd = this.onEnd.bind(this);
-    }
+    
 
     // Slides config
     slidePosition(slide) {
@@ -122,11 +120,31 @@ export default class Slide {
         if (this.index.next !== undefined) this.changeSlide(this.index.next)
     }
 
+    onResize() {
+        setTimeout(() => {
+            this.slidesConfig();
+            this.changeSlide(this.index.active);
+        }, 1000);
+    }
+
+    addResizeEvent() {
+        window.addEventListener('resize', this.onResize)
+    }
+
+    bindEvents() {
+        this.onStart = this.onStart.bind(this);
+        this.onMove = this.onMove.bind(this);
+        this.onEnd = this.onEnd.bind(this);
+        this.onResize = debaunce (this.onResize.bind(this), 200);
+    }
+
+
     init() {
         this.bindEvents();
         this.transition(true);
         this.addSlideEvents();
         this.slidesConfig();
+        this.addResizeEvent();
         return this;
     }
 }
